333b036d8955c8c37860ca292c4f6bb753673fd4
송기종 <megasoby@ssg.com>
2021-12-26 22:36:21 +0900

    3강 중간완료 소스 커밋

--- src/main/java/com/example/myrestful/helloworld/HelloWorldController.java
+++ src/main/java/com/example/myrestful/helloworld/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.example.myrestful.helloworld;
 
-import com.example.myrestful.helloworld.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
+
     // GET
     // "/hello-world" (endpoint)
     // @RequestMapping(method=RequestMethod.GET, path="/hello-world") : 구방식
@@ -25,4 +33,13 @@ public class HelloWorldController {
     public HelloWorldBean HelloWorldBean(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World Bean, %s", name));
     }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("greeting.message", null, locale);
+        //return messageSource.getMessage("")
+
+    }
+
 }
--- src/main/java/com/example/myrestful/user/User.java
+++ src/main/java/com/example/myrestful/user/User.java
@@ -3,12 +3,19 @@ package com.example.myrestful.user;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Data
 @AllArgsConstructor
+
 public class User {
     private Integer id;
+
+    @Size(min=2, message = "Name은 2글자 이상 등록해주세요!")
     private String name;
+
+    @Past
     private Date joinDate;
 }
